5d9ba2dffaef8f8b10e847f08b77617568415c1d
root <root@ip-172-31-5-26.us-east-2.compute.internal>
2019-06-07 16:40:42 +0000

    Adding to dir create

---
+++ dircreate/src/main/java/com/myproj/app/App.java
@@ -0,0 +1,13 @@
+package com.myproj.app;
+
+/**
+ * Create DirecCreate !
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Create Directory!!" );
+    }
+}
